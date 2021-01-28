e64e252a922e43e1affcfb16578cbd85392cb76b
Armin Baur <arminbaur92@gmail.com>
2021-01-29 00:01:19 +0100

    changed models name and modified the corresponding hyp_tun.sh

--- hyp_tun.sh
+++ hyp_tun.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-for cfg in yolov5_dvs_Covn7.yaml yolov5_dvs_focus_S.yaml; do
+for method in super tent heaviside tanh circ heavi_erfc; do
+  for cfg in dvs_F.yaml dvs_C1.yaml dvs_C3.yaml dvs_FCC.yaml dvs_FCBNCSPC.yaml; do
     for t in f t; do
-        for vrs in f t; do
-            command="python train.py --img 416 --batch 4 --epochs 2000 --data data.yaml --weights '' --cache --cfg  models/$cfg --wandblog True --project dvs_img --name super --tauskip $t --v_resetskip $vrs"
+      for vrs in f t; do
+            command="python train.py --img 416 --batch 13 --epochs 4000 --data data.yaml --weights '' --cache --cfg  models/$cfg --wandblog True --project dvs_$1 --name $method --tauskip $t --v_resetskip $vrs --spiking_method $method"
             echo $command
             $command
-        done
+
+      done
     done
+  done
 done
+

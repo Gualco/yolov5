81f722caa9bddfae698fd080a323525253abbd39
Armin Baur <arminbaur92@gmail.com>
2021-02-12 00:37:43 +0100

    refactoring and added cli command data

--- run_multiple.py
+++ run_multiple.py
@@ -5,11 +5,12 @@ import argparse
 from loguru import logger
 import time
 
-def gpu_stat_wait(free_memory:int=3000):
+def gpu_stat_wait_until_free(free_memory:int=3000)->int:
     gpustats = gpustat.GPUStatCollection.new_query()
     g_json = gpustats.jsonify()
     for i, g in enumerate(g_json["gpus"]):
         if(g["memory.total"]-g["memory.used"] > free_memory):
+            os.environ['CUDA_VISIBLE_DEVICES'] = f'{i}'
             return i
         else:
             # print("fucking fully loaded GPU")
@@ -30,25 +31,32 @@ def main():
         type=str,
         default="x2",
         help='project postfix displayed in wandb')
+    argparser.add_argument(
+        '-d', '--data',
+        type=str,
+        default="data.yaml",
+        help='path to data yaml description')
     args = argparser.parse_args()
 
     logger.info(vars(args))
-    index = gpu_stat_wait(args.free_memory)
+    index = -1
     while index < 0:
         time.sleep(1)
-        index = gpu_stat_wait(args.free_memory)
+        index = gpu_stat_wait_until_free(args.free_memory)
 
     os.environ['CUDA_VISIBLE_DEVICES'] = f'{index}'
     # os.system(f"export CUDA_VISIBLE_DEVICES={index}")
     time.sleep(0.1)
 
     for method in ["super", "tent", "heaviside", "tanh circ", "heavi_erfc"][:1]:
-      for cfg in ["dvs_FCBNCSPC.yaml", "dvs_FCBNCSPC_x2.yaml", "dvs_AllConv.yaml", "dvs_AllConv_x2.yaml", "dvs_F.yaml", "dvs_F_x2.yaml", "dvs_C1_x2.yaml",
+      for cfg in [ "dvs_F_x2.yaml", "dvs_F.yaml","dvs_FCBNCSPC.yaml", "dvs_FCBNCSPC_x2.yaml", "dvs_AllConv.yaml", "dvs_AllConv_x2.yaml", "dvs_C1_x2.yaml",
                   "dvs_C1_x2.yaml", "dvs_C3.yaml", "dvs_FCC.yaml"][:2]:
         for t in ["f", "t"][:1]:
           for vrs in ["f", "t"][:1]:
-            command="python train.py --img 416 --batch 13 --epochs 4000 --data data.yaml --weights '' " \
+            command=f"python train.py --img 416 --batch 13 --epochs 4000 --data {args.data} --weights '' " \
                         f"--cache --cfg  models/{cfg} --wandblog True --project dvs_{args.project} --name {method} --tauskip {t} --v_resetskip {vrs} --spiking_method {method}"
+            logger.info("running:")
+            logger.info(command)
             os.system(command)
 
 if __name__ == '__main__':

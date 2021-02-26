d57af0fc062b7c06f64c5fd873b2f037bd77bab6
Armin Baur <arminbaur92@gmail.com>
2021-02-26 12:43:29 +0100

    fixed test shapes

--- train.py
+++ train.py
@@ -413,7 +413,7 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
                                                  save_dir=save_dir,
                                                  plots=plots and final_epoch,
                                                  log_imgs=opt.log_imgs if wandb else 0,
-                                                 input_tensor_shape=input_tensor_shape)
+                                                 input_tensor_shape=(opt.time_seq_len, total_batch_size, 3, imgsz_test, imgsz_test))
 
             # Write
             with open(results_file, 'a') as f:
@@ -489,7 +489,8 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
                                           dataloader=testloader,
                                           save_dir=save_dir,
                                           save_json=save_json,
-                                          plots=False)
+                                          plots=False,
+                                          input_tensor_shape=(opt.time_seq_len, total_batch_size, 3, imgsz_test, imgsz_test))
 
     else:
         dist.destroy_process_group()
